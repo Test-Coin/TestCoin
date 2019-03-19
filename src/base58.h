3bfe58a24521c6082072716b81a6acb4b90beacf
MotoAcidic <travisfinch01@gmail.com>
2019-03-19 15:03:31 -0400

    More

--- src/base58.h
+++ src/base58.h
@@ -103,8 +103,8 @@ public:
 class CBitcoinAddress : public CBase58Data
 {
 public:
-    //bool Set(const CKeyID& id);
-    //bool Set(const CScriptID& id);
+    bool Set(const CKeyID& id);
+    bool Set(const CScriptID& id);
     bool Set(const CTxDestination& dest);
     bool IsValid() const;
     bool IsValid(const CChainParams& params) const;
